25b8e624d280fee6c08f573964ca092327a69a30
manikantabc <160829636+manikantabc@users.noreply.github.com>
2024-02-29 12:57:14 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,9 @@
+class add
+{
+public static void main(String[] args)
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+System.out.println("addition of "+a+"+"+b+"is"+(a+b));
+}
+}
