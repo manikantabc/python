c575352a38518391b420f1d24bc898276700de38
manikantabc <160829636+manikantabc@users.noreply.github.com>
2024-03-27 13:59:41 +0530

    Delete add.java

--- add.java
+++
@@ -1,9 +0,0 @@
-class add
-{
-public static void main(String[] args)
-{
-int a=Integer.parseInt(args[0]);
-int b=Integer.parseInt(args[1]);
-System.out.println("addition of "+a+"+"+b+"is"+(a+b));
-}
-}
